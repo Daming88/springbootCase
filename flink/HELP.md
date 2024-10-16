0246929cc64f0fdbdd4f87dbdbda2b2b5a9c165b
Daming <1306573135@qq.com>
2024-10-16 12:26:51 +0800

    feta

---
+++ flink/HELP.md
@@ -0,0 +1,9 @@
+## springboot基于mysql作为数据源集成flink实现数据的实时处理
+### Apache Flink 是一个在有界数据流和无界数据流上进行有状态计算分布式处理引擎和框架。Flink设计旨在所有常见的集群环境中运行，以任意规模和内存级速度执行计算。
+1. 环境准备\
+JDK1.8或者更高版本（这使用的是JDK17）\
+maven\
+Apache Flink\
+Spring Boot 3.X
+MySQL
+2. maven依赖\
---
+++ ip2region/src/test/java/com/daming/ip2region/util/IpUtilsTest.java
@@ -0,0 +1,37 @@
+package com.daming.ip2region.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IpUtilsTest {
+
+    @Test
+    public void getIp() {
+    }
+
+    @Test
+    public void getUserAgent() {
+    }
+
+    @Test
+    public void getCityInfo() {
+    }
+
+    @Test
+    public void getIp2region() {
+        System.out.println(IpUtils.getIp2region("114.114.114.114"));
+    }
+
+    @Test
+    public void removeLastPartAndAllPipes() {
+    }
+
+    @Test
+    public void getHostIp() {
+    }
+
+    @Test
+    public void getHostName() {
+    }
+}
